8af5c338381e278f16edf565a0376585ecaccdb7
Tanessia Gordon <nessiagordon@yahoo.com>
2015-10-14 11:08:51 -0400

    remove name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 const ListItem = React.createClass({
 
   handleClick: function() {
-    // TODO: Remove this
-    console.log(`You clicked ${this.props.name}!`);
+    // console.log(`You clicked ${this.props.name}!`);
+    this.props.onRemoveName(this.props.name.index);  //onRemoveName is now a property (see below)
+                                                    // index is added as prop also that allows the removeName function to remove the name
+                                                    //that was clicked on.
   },
-
-  render: function() {
-
+    render: function() {
     return (
       <li onClick={this.handleClick}>{this.props.name}</li>
     );
@@ -39,8 +40,16 @@ const ListMaker = React.createClass({
   },
 
   addName: function (newName) {
-    let names = this.state.names;
+    let names = this.state.names.slice();
     names.push(newName);
+    this.setState({
+      names: names
+    });
+  },
+
+  removeName: function (index, i) {
+    let names = this.state.names.slice();
+    names.splice(index, 1);
     this.setState({
       names: names
     });
@@ -48,8 +57,8 @@ const ListMaker = React.createClass({
 
   render: function() {
     let listItems = this.state.names.map((item, i) => {
-      return <ListItem key={i} name={item} />
-    });
+      return <ListItem key={i} index={i} name={item} onRemoveName={this.removeName} />    //add removeName function as a prop
+    });                                                                         // so that it can be added to the handleClick function
     return (<div>
       <h1>List Maker</h1>
       <ul>
